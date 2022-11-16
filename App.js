9cc9ead50eaf5506df8e891fc77b693a0ded70f5
TeGun13 <te.kunyanut_st@tni.ac.th>
2022-11-16 13:48:25 +0700

    Commit 2

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Information from "./components/Information";
 import Welcome from "./components/Welcome";
+import Txtinput from "./components/Txtinput";
+import CustomComponent from "./components/CustomComponent";
 
 const App = () => {
   return (
     <View>
       {/* <Information /> */}
 
-      <Welcome></Welcome>
+      {/* <Welcome></Welcome> */}
+
+      {/* <Txtinput></Txtinput> */}
+      <CustomComponent></CustomComponent>
     </View>
   );
 };
---
+++ components/CustomComponent.js
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Cat = () => {
+  return (
+    <View>
+      <Text>I LOVE CAT</Text>
+    </View>
+  );
+};
+
+const CustomComponent = () => {
+  return (
+    <View>
+      <Text>CustomComponent</Text>
+      <Cat></Cat>
+    </View>
+  );
+};
+
+export default CustomComponent;
