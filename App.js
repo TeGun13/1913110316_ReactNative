2ab76adb7bc1ab321344d07c78e44e0510ba6bf2
TeGun13 <te.kunyanut_st@tni.ac.th>
2022-11-30 13:08:11 +0700

    Practice-Week4

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ import CustomComponent from "./components/CustomComponent";
 import Logo from "./components/Logo";
 import Lots from "./components/Lots";
 import MyCustomTextWith from "./components/MyCustomTextWith";
+import Count from "./components/Count";
+import TxtInput1 from "./components/TxtInput1";
+import UserNamePassword from "./components/UserNamePassword";
 
 const App = () => {
   return (
@@ -20,9 +23,18 @@ const App = () => {
 
       {/* <Logo></Logo> */}
       {/* <Lots></Lots> */}
-      <MyCustomTextWith></MyCustomTextWith>
+      {/* <MyCustomTextWith></MyCustomTextWith> */}
+      {/* <Count num={2} title="Click"></Count> */}
+      {/* <TxtInput1/> */}
+      <UserNamePassword></UserNamePassword>
     </View>
   );
 };
-
 export default App;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+// });
---
+++ components/UserNamePassword.js
@@ -0,0 +1,71 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  SafeAreaView,
+} from "react-native";
+import React, { useState } from "react";
+
+const UserNamePassword = () => {
+  const [userName, TextInputName] = useState("");
+  const [userEmail, TextInputEmail] = useState("");
+
+  const chechTextInput = () => {
+    if (!userName.trim()) {
+      alert("Please Enter Name");
+      return;
+    } else if (!userEmail.trim().trim()) {
+      alert("Please Enter Email");
+      return;
+    } else {
+      alert("Success");
+      return;
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TextInput
+          value={userName}
+          onChangeText={(userName) => TextInputName(userName)}
+          placeholder={"Input Username"}
+          style={styles.textInputStyle}
+        />
+        <TextInput
+          value={userEmail}
+          onChangeText={(userEmail) => TextInputEmail(userEmail)}
+          placeholder={"Input Email"}
+          style={styles.textInputStyle}
+        />
+
+        <View style={{ marginTop: 10 }}>
+          <Button
+            title="SUBMIT"
+            onPress={() => {
+              chechTextInput();
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default UserNamePassword;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35,
+  },
+  textInputStyle: {
+    width: "100%",
+    height: 40,
+    paddingHorizontal: 5,
+    borderWidth: 0.5,
+    marginTop: 15,
+  },
+});
