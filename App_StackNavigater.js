245946167dba18cbabced9d9818becd6784c260e
TeGun13 <te.kunyanut_st@tni.ac.th>
2022-12-21 13:34:23 +0700

    Header Button

---
+++ App_StackNavigater.js
@@ -0,0 +1,71 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
+
+// import HomePost from "./screens/HomePost";
+// import CreatePost from "./screens/CreatePost";
+
+// import FirstPage from "./screens/FirstPage";
+// import SecondPage from "./screens/SecondPage";
+
+// import FirstPage from "./pages/FirstPage";
+// import SecondPage from "./pages/SecondPage";
+// import ThirdPage from "./pages/ThirdPage";
+
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="เกี่ยวกับเรา"
+//         onPress={() => navigation.navigate("About")}
+//       />
+//     </View>
+//   );
+// }
+
+// function AboutScreen() {
+//   return (
+//     <View style={{ flex: 1, alignContent: "center", justifyContent: "center" }}>
+//       <Text>About Screen</Text>
+//     </View>
+//   );
+// }
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#db7093",
+          },
+          headerTintColor: "#ffff",
+
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name="About" component={AboutScreen}></Stack.Screen>
+
+        {/* <Stack.Screen name="HomePost" component={HomePost}></Stack.Screen>
+        <Stack.Screen name="CreatePost" component={CreatePost}></Stack.Screen>
+
+        <Stack.Screen name="FirstPage" component={FirstPage}></Stack.Screen>
+        <Stack.Screen name="SecondPage" component={SecondPage}></Stack.Screen>
+        <Stack.Screen name="ThirdPage" component={ThirdPage}></Stack.Screen>  */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,51 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
 import { Ionicons, Feather } from "@expo/vector-icons";
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+  HiddenItem,
+  OverflowMenu,
+} from "react-navigation-header-buttons";
+
+const IoniconsHeaderButton = (props) => (
+  // the `props` here come from <Item ... />
+  // you may access them and pass something else to `HeaderButton` if you like
+  <HeaderButton
+    IconComponent={Ionicons}
+    iconSize={30}
+    color="white"
+    {...props}
+  />
+);
 
 const HomeScreen = ({ navigation }) => {
+  React.useEffect(() => {
+    // Use `setOptions` to update the button that we previously specified
+    // Now the button includes an `onPress` handler to update the count
+    navigation.setOptions({
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => alert("เมนูคำสั่ง")}
+          />
+        </HeaderButtons>
+      ),
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+            title="person-add"
+            iconName="person-add"
+            onPress={() => alert("ลงทะเบียน")}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Ionicons name="home" size={24} color="#008b8b" />
